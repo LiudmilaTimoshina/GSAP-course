296e37e3b07f44f067946ba2ebc4310f816c0914
Liudmila T <liudmila.timoshina@gmail.com>
2025-04-24 16:38:11 +0300

    Добавлена папка test как часть проекта

---
+++ lesson3/lesson3.js
@@ -0,0 +1,8 @@
+const titleProject = 'Оценка стоимости';
+const screensValue = 'шаблонные, с уникальным дизайном, с анимациями';
+const screenPrice = 2345;
+const percentage = 10;
+const fullPrice = 500;
+const responsive = true;
+
+console.log(titleProject, screensValue, screenPrice, percentage, fullPrice, responsive )
