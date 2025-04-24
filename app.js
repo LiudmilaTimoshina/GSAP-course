598ba186b521651260ddf2865cb833d5c19e7fd8
Liudmila T <liudmila.timoshina@gmail.com>
2025-04-24 16:38:11 +0300

    Добавлена папка test как часть проекта

---
+++ app.js
@@ -0,0 +1,33 @@
+// const firstName = 'Liudmila'
+// firstName = 'Amy'
+// console.log('Hello', firstName)
+
+const age = 27;
+console.log(typeof age)
+
+const bigInt = 2987453643n;
+console.log(typeof bigInt)
+
+const firstName = 'Liu';
+console.log(typeof firstName)
+
+let age2;
+console.log(typeof age2)
+
+
+const obj = {
+    name: 'Liu',
+    age: 40,
+    isWorked: true
+
+}
+
+const num = 10
+const num2 = 5
+
+console.log(num + num2);
+console.log(num - num2);
+console.log(num * num2);
+console.log(num / num2);
+console.log(num % num2)
+
